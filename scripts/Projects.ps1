013987d22d796b8eafb717e9a6162179a5c3c09c
Jesper Otto Mønsted <37215959+JesperMonsted@users.noreply.github.com>
2021-03-17 21:09:50 +0100

    Update Projects.ps1

--- scripts/Projects.ps1
+++ scripts/Projects.ps1
@@ -1,8 +1,2 @@
-$folder = "C:\git\code"
-New-Item $folder -ItemType Directory
-Set-location $folder
-git clone https://github.com/casz/dotfiles.git
-git clone https://github.com/casz/setup.git
-
 $folder = "C:\git\work"
 New-Item $folder -ItemType Directory
