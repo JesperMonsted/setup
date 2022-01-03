86fb99faaaf855252f2fdfd8f4795e25bfa99d25
Jesper Otto Mønsted <37215959+JesperMonsted@users.noreply.github.com>
2022-01-03 18:52:24 +0100

    Update Projects.ps1

--- scripts/Projects.ps1
+++ scripts/Projects.ps1
@@ -2,8 +2,8 @@ $folder = "C:\git\code"
 if ([System.IO.File]::Exists("${folder}") -eq $false) {
   New-Item $folder -ItemType Directory
 }
-git clone https://github.com/jetersen/dotfiles.git "${folder}\dotfiles"
-git clone https://github.com/jetersen/setup.git "${folder}\setup"
+git clone https://github.com/JesperMonsted/dotfiles.git "${folder}\dotfiles"
+git clone https://github.com/JesperMonsted/setup.git "${folder}\setup"
 
 $folder = "C:\git\work"
 if ([System.IO.File]::Exists("${folder}") -eq $false) {
