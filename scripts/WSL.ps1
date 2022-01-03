b2d4af37f7de42e8ce3b326cc00fb30fd0ff8643
Jesper Otto Mønsted <37215959+JesperMonsted@users.noreply.github.com>
2022-01-03 18:58:36 +0100

    Update WSL.ps1

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -3,5 +3,5 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
   winget install --exact --silent "Canonical.Ubuntu.2004"
   ubuntu2004.exe install --root
   Ubuntu2004 run "curl -sL '$helperUri/WSL.sh' | bash"
-  Ubuntu2004 config --default-user joseph
+  Ubuntu2004 config --default-user jesper
 }
